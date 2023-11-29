a39d70885c9c2f08841647a609be838d3e5ee717
Vasanth P <131950066+Vasanth-p@users.noreply.github.com>
2023-11-29 21:32:33 +0530

    TowerOfHonoi
    Tower of Hanoi Solver
    This Python script provides solutions to the classic Tower of Hanoi problem using both Depth-First Search (DFS) and Breadth-First Search (BFS) approaches.
    
    Features
    DFS Approach: Recursively solves the Tower of Hanoi problem by moving disks from the source peg to the destination peg using an auxiliary peg.
    BFS Approach: Uses a breadth-first search strategy to find the optimal solution for the Tower of Hanoi problem.
    How to Use
    Run the script.
    Enter the number of discs when prompted.
    The script will output the steps taken to solve the Tower of Hanoi problem using both DFS and BFS approaches.

---
+++ Towerofhonoi.py
@@ -0,0 +1,38 @@
+from collections import deque
+def tower_of_hanoi_dfs(n, source, auxiliary, destination):
+    if n == 1:
+        print(f"Move disc 1 from {source} to {destination}")
+        return
+    tower_of_hanoi_dfs(n - 1, source, destination, auxiliary)
+    print(f"Move disc {n} from {source} to {destination}")
+    tower_of_hanoi_dfs(n - 1, auxiliary, source, destination)
+def tower_of_hanoi_bfs(n, source, destination):
+    class State:
+        def __init__(self, pegs, steps=""):
+            self.pegs, self.steps = pegs, steps
+    initial_state = State([[i for i in range(n, 0, -1)], [], []])
+    visited, queue = set(), deque([initial_state])
+    while queue:
+        state = queue.popleft()
+        if state.pegs[2] == [i for i in range(n, 0, -1)]:
+            return state.steps
+        for source in range(3):
+            for destination in range(3):
+                if source != destination and state.pegs[source]:
+                    new_pegs = [peg[:] for peg in state.pegs]
+                    disc = new_pegs[source].pop()
+                    new_pegs[destination].append(disc)
+                    new_state = State(new_pegs, state.steps + f"Move disc {disc} from {chr(65+source)} to {chr(65+destination)}\n")
+                    if new_state not in visited:
+                        visited.add(new_state)
+                        queue.append(new_state)
+                        return "No solution found."
+def main():
+    n = int(input("Enter the number of discs: "))
+    print("\nBFS Approach:")
+    bfs_result = tower_of_hanoi_bfs(n, 'A', 'C')
+    print(bfs_result)
+    print("\nDFS Approach:")
+    tower_of_hanoi_dfs(n, 'A', 'B', 'C') 
+if __name__ == "__main__":
+    main()
